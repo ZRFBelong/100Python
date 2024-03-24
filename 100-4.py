9d2db05af8abf5f7f85bff1f9eed642ce9a29a03
ZRFBelong <80340941@qq.com>
2024-03-24 08:49:33 +0800

    添加学习网站的link

--- 100-4.py
+++ 100-4.py
@@ -1,10 +1,17 @@
+#!/usr/bin/python3
+#def square(a):
+#    for i in a:
+#        i = lambda i : i**2
+
 numbers = [1,2,3,6,99,8,5,7]
 maxim= max(numbers)
 print(maxim)
 numbers.sort()
 print(numbers)
-
-import random
-
-randoms=random()
+for i in numbers:
+    i = lambda i : i**2
+sortednumber=[]
+#numbers.sort(key=square)
+print(numbers)
+#print(help(list.sort))
         
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-Normally Python Exercise
+Normally Python Exercise Reference： 娟子老师https://atxunda.com/
 20240321 update to 14
+
